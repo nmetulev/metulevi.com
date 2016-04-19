a13ac6577e7244196e49ec0b0283ecdb88a25165
Nikola Metulev <nikolame@microsoft.com>
2016-04-18 23:49:37 -0700

    updated pull.php

--- tools/pull.php
+++ tools/pull.php
@@ -1,6 +1,8 @@
 <?php
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 if ( $_POST['payload'] ) {
-  shell_exec( 'cd .. && git reset --hard HEAD && git pull' );
+  echo shell_exec( 'cd .. && git reset --hard HEAD && git pull' );
 }
-?>hi
+else
+  echo "Hi there!";
+?>
